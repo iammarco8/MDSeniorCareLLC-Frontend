0489a4f1300bf3546e67ee8dfb283b4b1524b64a
Marco Duhaney <marco.duhaney2023@gmail.com>
2025-02-23 02:35:21 -0500

    background color change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MiddleComponent } from './partials/middle/middle.component';
+import { SecondMiddleComponent } from './partials/second-middle/second-middle.component';
 
 
 const routes: Routes = [
-  {path:'home', title:'Home', component:MiddleComponent,pathMatch:'full'}
+  {path:'home', title:'Home', component:MiddleComponent,pathMatch:'full'},
+  {path:'secondDraft', title:'SecondDraft', component:SecondMiddleComponent,pathMatch:'full'}
 ];
 
 @NgModule({
